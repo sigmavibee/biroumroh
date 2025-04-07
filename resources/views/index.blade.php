0b8f9029e4c06b3beedc2424e2b6044ed9abe13e
sigmavibee <rizkyrn027@gmail.com>
2025-04-07 14:59:51 +0700

    apakah bisa? part 3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ asset('build/assets/app-041e359a.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('build/assets/app-041e359a.css') }}" rel="stylesheet"> --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('build/assets/assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{asset('css/custom.css') }}">
-    <link rel="stylesheet" href="{{asset('css/custom-paket.css')}}">
-    <link rel="stylesheet" href="{{asset('css/custom-biro.css')}}">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/custom.css">
+    <link rel="stylesheet" href="/css/custom-paket.css">
+    <link rel="stylesheet" href="/css/custom-biro.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
